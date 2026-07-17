f003cec5abd9b1811418a6cc7b301e5eaa44a1fb
Kostiantyn <kog@yousee.dk>
2026-07-17 09:01:59 +0300

    revert backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-devops-homework-5"
-#     key            = "lesson-5/terraform.tfstate"
-#     region         = "eu-north-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-devops-homework-5"
+    key            = "lesson-5/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
